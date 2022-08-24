a8bbf4339901e7c78d0a830ba81fc7a7890431dc
Valentin Barit <valentin.barit@gmail.com>
2022-08-24 09:47:54 +0200

    feat: 🎸 rename branches

--- src/index.sh
+++ src/index.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-VERSION_NUM=0.1.0
+VERSION_NUM=0.2.0
 
 #%include checkbox_input.sh
+#%include text_input.sh
 
 branches=($(git branch | tr -d " *"))
 checkbox_input "Which branch do you want to delete?" branches selected_branches
@@ -11,4 +12,16 @@ if [ -z "$selected_branches" ]; then
   echo "No branch to delete."
 else
   git branch -D $(join selected_branches | tr -d ",")
+fi
+
+branches=($(git branch | tr -d " *"))
+checkbox_input "Which branch do you want to rename?" branches selected_branches
+
+if [ -z "$selected_branches" ]; then
+  echo "No branch to rename."
+else
+  for selected_branch in $(join selected_branches | tr -d ","); do
+    text_input "What's the new name for branch $selected_branch?" name
+    git branch -m $selected_branch $name
+  done
 fi
